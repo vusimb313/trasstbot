0a27b87db6f65957797f733709ca8d9c6517d9f4
Sandile Dlamini <30494888+devsandile@users.noreply.github.com>
2018-07-12 19:22:13 +0200

    Removed invalid webhook token
    The user implementing this must supply his own which he obtains at https://www.developers.facebook.com

--- config.php
+++ config.php
@@ -11,5 +11,5 @@
 
 return [
     'webhook_verify_token' => 'WEBHOOK_VERIFY_TOKEN',
-    'access_token'         => 'EAABoAuIYHmoBAFZAxnOd0ZBwqIjoYHrTu1YLBJtt8XLZAQpG6xiyfqkNqC7ZB1qSZCZBvve5LQbto39c9FgntraK5BbAilGuHZAXxhDWS9dSRCXFxPC4EonaPDlcplZCxJ1ZB4fzpEX3BYuPgZCNxDVCeIZAuYtkPDlEPsg7sztwmY5LwZDZD'
+    'access_token'         => 'YOUR_TOKEN_HERE'
 ];
